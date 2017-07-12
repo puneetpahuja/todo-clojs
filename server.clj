01bcfab6b2b027ecb1aedf1b73a1107151993930
p4v4n <pavan.barcelona@gmail.com>
2017-07-12 20:08:08 +0530

    Create server and add get method

--- server.clj
+++ server.clj
@@ -16,6 +16,35 @@
 
 (console.log todos)
 
+(def server 
+  (http.createserver 
+    (fn [req res] 
+      (def filepath (+ "./public" req.url))
+      (fs.readFile filepath (fn [err data]
+                  (if err
+                     (do (assign res.statusCode 404)
+                         (res.end "Page not found")))
+	              (assign res.statusCode 200)
+	              (res.end data)))
+	  (def parsedUrl (url.parse req.url))
+	  (def parsedQuery (querystring.parse parsedUrl.query))
+	  (def method req.method)
+	  (if (== method "GET")
+	      (if (= (request.url.indexOf "/todos") 0)
+	          (do (let localTodos todos)
+	              (res.setHeader "Content-Type" "application/json")
+	              (if parsedQuery.searchtext
+	                  (do (assign localTodos (localTodos.filter (fn [obj]
+	                      (def temp (obj.message.toLowerCase))
+	                      (>= (temp.indexOf (parsedQuery.searchtext.toLowerCase)) 0))
+	                                		     	 	 )))
+	                  (res.end (JSON.stringfy localTodos))))
+	          (res.end (JSON.stringfy localTodos)))
+	      (res.end (JSON.stringfy localTodos)))
+	)))
+
+(server.listen 3001)
+
 
 
 
